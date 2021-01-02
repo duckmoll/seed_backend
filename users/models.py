bd8211f0f422c90b18a4dabebf658e80810eb96f
duckmoll <ljlin.951010@gmail.com>
2021-01-02 16:44:00 -0500

    change

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 # An account model
 class Account(models.Model):
     birthday = models.DateField(blank=True, null=True)
